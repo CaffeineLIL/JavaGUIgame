007cabfa7443ba3817d380b15fbc143ca12e121f
CaffeineLIL <lightme45@naver.com>
2024-05-28 01:42:56 +0900

    벽 구현중

--- JavaGUiGame/src/stage/stage_1.java
+++ JavaGUiGame/src/stage/stage_1.java
@@ -33,7 +33,7 @@ public class stage_1 extends JPanel {
         WeakEnemy enemy_1 = new WeakEnemy(playerX, playerY);
         enemy1_hp = enemy_1.getenHp();
         add(enemy_1);
-
+        
         // 타이머 설정 (적의 이동을 주기적으로 업데이트하기 위해)
          timer = new Timer(10, new ActionListener() {
             @Override
@@ -52,16 +52,35 @@ public class stage_1 extends JPanel {
         });
         timer.start();
     }
-
+    
+    // 투명한 벽을 그리는 메서드 추가
+    private void drawWalls(Graphics2D g2d) {
+    	//위 , 아래
+        g2d.setColor(new Color(23, 79, 0, 200)); // 투명한 색상 : 0, 0, 0, 0
+        g2d.fillRect(80, 20, 1000, 50); // 예시: (100, 100) 위치에 가로 1000, 세로 50 크기의 투명한 사각형 그리기
+        g2d.fillRect(80, 520, 1000, 50);
+        
+        //왼쪽, 오른쪽
+        g2d.fillRect(40, 20, 50, 500); 
+        g2d.fillRect(1005, 20, 50, 500); 
+        // 필요한 만큼 더 많은 사각형을 추가하여 벽을 만듭니다.
+    }
+    
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        Graphics2D g2d = (Graphics2D) g.create();
+
         // 배경 이미지 그리기
         if (backgroundImage != null) {
-            // 이미지를 JPanel에 꽉 차도록 그립니다.
-            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+            g2d.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
         }
+
+        // 투명한 벽 그리기
+        drawWalls(g2d);
+
+        g2d.dispose();
     }
 
     @Override
